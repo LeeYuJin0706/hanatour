5c6758a31bcd22796de15974095bb3ab8e001af9
LeeYuJin0706 <leeyujin0706@naver.com>
2022-01-18 14:56:30 +0900

    학원
    0118

---
+++ ex.js
@@ -0,0 +1,60 @@
+let index = 1;
+let moveCheck = true;
+const sliderBox = document.querySelector('.slider-box');
+const slideContents = document.querySelectorAll('.slide');
+const sliderLength = slideContents.length;
+
+let copyFirst = sliderBox.firstElementChild.cloneNode(true);
+let copylast = sliderBox.lastElementChild.cloneNode(true);
+
+sliderBox.appendChild(copyFirst);
+sliderBox.insertBefore(copylast, sliderBox.firstElementChild);
+
+sliderBox.style.width = 100 * (sliderLength+2) + "vw";
+sliderBox.style.transform = "translateX(-" + 100*(index) + "vw)";
+
+
+
+function slideToLeft(){
+    console.log("왼쪽")
+    if(moveCheck){
+        moveCheck = false;
+        
+        index--;
+        moveSlide(1000);
+        
+        setTimeout(function(){
+            if(index===0){
+                index=sliderLength;
+                moveSlide(0);
+            }
+            moveCheck=true;
+            moveSlide(0)
+        }, 1000);
+    }
+}
+
+function slideToRight(){
+    console.log("오른쪽")
+    if(moveCheck){
+        moveCheck=false;
+        
+        index++;
+        moveSlide(1000);
+        
+        setTimeout(function(){
+            if(index===sliderLength+1){
+                index=1;
+                moveSlide(0);
+            }
+            
+            moveCheck=true;
+            moveSlide(0)
+        }, 1000);
+    }
+}
+
+function moveSlide(time){
+    sliderBox.style.transition = time +'ms';
+    sliderBox.style.transform = "translateX(-" + 100*(index) +"vw)";
+}
